75fb149663303868d3edf186ff98a7c65cf25562
shifan <shifan@tower5.(none)>
2016-08-01 17:26:02 -0700

    updated compares

--- mkfigures/compares.m
+++ mkfigures/compares.m
@@ -7,7 +7,8 @@ PLOTEXP = 0;      % plot experiment structure factors
 PLOTQS = 1;      % plot inverse peak intensities and peak locations
 
 % plot parameters
-NREP = [1:55];  % simulation plot range
+NREP1 = [1:6:55];  % simulation plot range for structure factors
+NREP2 = [1:55];    % simulation plot range for peaks in structure factors
 NEXP = 3:2:9;   % experiment plot range
 
 % load Simulation CHI parameters
@@ -18,24 +19,24 @@ SCALE = 1/46;     % scaling of simulation structure factor
 DISCR = 0;        % if only plot s(k) at selective k values
 
 % add/define paths
-%dir = '../data/';       % data directory
-%dir = '../../../quinn/MCPoly/MCparallelMPI/data/';
+loaddir = '../data/';       % data directory
+loaddir = '../../../quinn/MCPoly/MCparallelMPI/lamNeg1_r7_27_16/';
 savedir = 'savedata/';   % save directory
 
 %% Figure 1: structure factors
-CHI = load(strcat(dir,'chi'));  % adjusted CHI values
+CHI = load(strcat(loaddir,'chi'));  % adjusted CHI values
 CHI = CHI(end,2:end);
 
 % load simulation data
 figure('Position', [100, 100, 1200, 900]);
-hold;set(gca,'fontsize',20)
+hold;set(gca,'fontsize',20);leg = {};
 
 if (PLOTSIM)
-    for REP = NREP
-      if max(NREP) == 1
+    for REP = NREP1
+      if max(NREP1) == 1
         col = 1;
       else
-        col = (REP-1)/(max(NREP)-1);
+        col = (REP-1)/(max(NREP1)-1);
       end
       SAVEFILENAME = sprintf('SSIM_CHIG%.3fLAM%.2fEPS%.2fFA%.2f',CHI(REP)*G,LAM,EPS,FA);
       ssim = load(strcat(savedir,SAVEFILENAME));
@@ -46,6 +47,28 @@ if (PLOTSIM)
       else
         plot(ssim(:,1),ssim(:,2),'-','linewidth',1.5,'color',[col 0 1-col])
       end
+      leg{REP} = strcat('\chiG = ', sprintf('%.2f', CHI(REP)*G));
+    end
+end
+
+if (PLOTMF)
+    % Plot mean-field theory
+    % load MF results
+    filename = sprintf('../utility/sdata/S_EPS%.3fLAM%.2fFA%.2f',EPS,LAM,FA);
+    MF = load(filename);
+    CHIS = MF(1,1);   %spinodal
+    for REP=NREP1
+        if max(NREP1)==1
+            col = 1;
+        else
+            col = (REP-1)/(max(NREP1)-1);
+        end
+
+        if CHI(REP) < CHIS/G
+            K = MF(2:end,1); %wavevectors
+            S = 1./(-2*CHI(REP)+EPS*MF(2:end,2));
+            plot(K,S,'--','color',[col 0 1-col],'linewidth',2);
+        end
     end
 end
 
@@ -57,38 +80,18 @@ if (PLOTEXP)
 
     cnt = 1;
     for ii = NEXP
-      if length(NREP) == 1
+      if length(NREP1) == 1
         col = 1;
       else
-        col = (cnt-1)/(length(NREP)-1);
+        col = (cnt-1)/(length(NREP1)-1);
       end
       plot(sexp_full(:,1)*rm,sexp_full(:,ii+1)*SCALE,'linewidth',3,'color',[col 0 1-col])
       cnt=cnt+1;
     end
 end
 
-if (PLOTMF)
-    % Plot mean-field theory
-    % load MF results
-    filename = sprintf('../utility/sdata/S_EPS%.3fLAM%.2fFA%.2f',EPS,LAM,FA);
-    MF = load(filename);
-    CHIS = MF(1,1);   %spinodal
-    for REP=NREP
-        if max(NREP)==1
-            col = 1;
-        else
-            col = (REP-1)/(max(NREP)-1);
-        end
-
-        if CHI(REP) < CHIS/G
-            K = MF(2:end,1); %wavevectors
-            S = 1./(-2*CHI(REP)+EPS*MF(2:end,2));
-            plot(K,S,'--','color',[col 0 1-col],'linewidth',2);
-        end
-    end
-end
-
 % plot process
+legend(leg{NREP1});
 axis([0.5,15,1e-2,5e3])
 xlabel('R_Mq');ylabel('S(q)');box on
 set(gca,'xscale','log');set(gca,'yscale','log')
@@ -104,8 +107,8 @@ if (PLOTQS)
   KS = MF(1,2);     % critical wavemode
   plot(CHI*G,2*(CHIS/G-CHI),'k-.','linewidth',2)
 
-  for REP = NREP
-    col = (REP-1)/(max(NREP)-1);
+  for REP = NREP2
+    col = (REP-1)/(max(NREP2)-1);
     SAVEFILENAME = sprintf('SSIM_CHIG%.3fLAM%.2fEPS%.2fFA%.2f',CHI(REP)*G,LAM,EPS,FA);
     ssim = load(strcat(savedir,SAVEFILENAME));
     plot(CHI(REP)*G,1/max(ssim(:,2)),'o','linewidth',1.5,'color',[col 0 1-col])
@@ -115,8 +118,8 @@ if (PLOTQS)
 
   subplot(2,1,2);hold;set(gca,'fontsize',20)
   plot(CHI*G,repmat(KS,length(CHI),1),'k-.','linewidth',2)
-  for REP = NREP
-    col = (REP-1)/(max(NREP)-1);
+  for REP = NREP2
+    col = (REP-1)/(max(NREP2)-1);
     SAVEFILENAME = sprintf('SSIM_CHIG%.3fLAM%.2fEPS%.2fFA%.2f',CHI(REP)*G,LAM,EPS,FA);
     ssim = load(strcat(savedir,SAVEFILENAME));
     QS = find(ssim(:,2) == max(ssim(:,2)));
