b57bf04694544b6dafe59db0f7c01fb6d1f51d23
qmacAstanford <qmac@stanford.edu>
2017-02-24 11:36:00 -0800

    Added correct input mu

--- code/MCcode/getRepusionData.f95
+++ code/MCcode/getRepusionData.f95
@@ -5,17 +5,17 @@ implicit none
 TYPE(MCvar), intent(inout) :: mc
 integer I
 
-mc%nDiameters = 3
-mc%nPhiValues = 3
-mc%phiMax=0.3
-mc%dmax=0.25
+mc%nDiameters = 31
+mc%nPhiValues = 200
+mc%phiMax=1.0
+mc%dmax=0.3
 mc%phiMin=0.0
 mc%dMin=0.01
 
 
 ALLOCATE(mc%ERepusionData(mc%nPhiValues,mc%nDiameters))
 
-OPEN (UNIT = 1, FILE = "../phiData", STATUS = 'OLD')      
+OPEN (UNIT = 1, FILE = "input/mu", STATUS = 'OLD')      
 DO I=1,mc%nPhiValues
     read(1,*)  mc%ERepusionData(I,:)
 enddo 
